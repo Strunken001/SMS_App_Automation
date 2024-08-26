0045644de8ab4ccf1003a31a6a8b3e94b56e8151
Strunken001 <59044902+Strunken001@users.noreply.github.com>
2024-08-26 09:29:11 +0100

    Update EmailSender.py

--- shared/EmailSender.py
+++ shared/EmailSender.py
@@ -4,23 +4,23 @@ from email.mime.text import MIMEText
 
 def send_email(user_account, mobile_numbers, message_content, to_email):
     # Set up the server
-    server = smtplib.SMTP('smtp.gmail.com', 587)  # Use your SMTP server and port
+    server = smtplib.SMTP('smtp.titan.email', 465)  # Use your SMTP server and port
     server.starttls()
 
     # Login to your email account
-    from_email = 'fahadbinazizconsultant@gmail.com'
-    password = 'lgny rqcw nwoy vlrd'
+    from_email = 'testnotifications@bullionsms.com'
+    password = 'Aramide@1990kohh'
     server.login(from_email, password)
 
     # Create the email
     msg = MIMEMultipart()
     msg['From'] = from_email
     msg['To'] = to_email
-    msg['Subject'] = "Test Automation Notification Email from SMS_Automation_App"
+    msg['Subject'] = "Test Automation Notification Email from Bullion_SMS_Automation_App"
 
     # Format the email body
     mobile_numbers_str = mobile_numbers
-    body = f"Dear Team,\n\nA test was successfully automated on User account: {user_account} to mobile numbers: {mobile_numbers_str} with content: {message_content}.\n\nBest Regards,\nYour Automation Team"
+    body = f"Dear Team,\n\nA test was successfully automated on User account: {user_account} to mobile numbers: {mobile_numbers_str} with content: {message_content}.\n\nBest Regards,\nBullion Automation Team"
     msg.attach(MIMEText(body, 'plain'))
 
     # Send the email
